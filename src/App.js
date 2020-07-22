ea9d05e8c3d3f2029bdd5fc287f669b5dcb46486
wuyaheng <wuyaheng2016@gmail.com>
2020-07-22 19:29:24 -0400

    create project component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-// import Portfolio from './pages/Portfolio/Portfolio';
+import Portfolio from './pages/Portfolio/Portfolio';
 // import Resume from './pages/Resume/Resume';
 // import Blog from './pages/Blog/Blog';
 
@@ -14,9 +14,9 @@ function App() {
 
     <BrowserRouter>
       <Route exact path='/' component={Home}/>
-      {/* <Route path='/portfolio' component={Portfolio}/> 
-      <Route path='/resume' component={Resume}/>
-      <Route path='/blog' component={Blog}/>  */}
+      <Route path='/portfolio' component={Portfolio}/> 
+      {/* <Route path='/resume' component={Resume}/>
+      <Route path='/blog' component={Blog}/>   */}
     </BrowserRouter> 
 
     </div>
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -5,7 +5,20 @@ import 'animate.css';
 function Project(props) {
         return (
             <div className="col-md-4 justify-content-center">
-
+            <div className="card">
+                <div className={styles.topCard}>
+                <h2 className={styles.title}>Título do card</h2>
+                <span className={styles.secondText}>Exemplo de texto secundário</span>
+                </div>
+                <div className={styles.mediaCard}></div>
+                <div className={styles.bottomCard}>
+                <p className={styles.bottomText}>Aqui é um texto exemplo de resumo da página ou do documento que o card trata.</p>
+                <div className={styles.actionsCard}>
+                    <button className={styles.actions}>Curtir</button>
+                    <button className={styles.actions}>Compartilhar</button>
+                </div>
+                </div>
+            </div>
             </div>
         )
 }
