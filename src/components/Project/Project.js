ea946837b33fdafdeab71ede9ec7ff67faf5faa5
wuyaheng <wuyaheng2016@gmail.com>
2020-07-22 19:50:36 -0400

    create project card

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -7,19 +7,18 @@ function Project(props) {
             <div className="col-md-4 justify-content-center">
             <div className="card">
                 <div className={styles.topCard}>
-                <h2 className={styles.title}>Título do card</h2>
-                <span className={styles.secondText}>Exemplo de texto secundário</span>
+                <h4 className="text-center">Título do card</h4>
                 </div>
                 <div className={styles.mediaCard}></div>
                 <div className={styles.bottomCard}>
                 <p className={styles.bottomText}>Aqui é um texto exemplo de resumo da página ou do documento que o card trata.</p>
-                <div className={styles.actionsCard}>
-                    <button className={styles.actions}>Curtir</button>
-                    <button className={styles.actions}>Compartilhar</button>
+                <div className="text-center">
+                    <button className={styles.actions}><i class="fab fa-internet-explorer"></i> VISIT SITE</button>
+                    <button className={styles.actions}><i class="fab fa-github"></i> GITHUB</button>
                 </div>
-                </div>
+             </div>
             </div>
-            </div>
+         </div>
         )
 }
 
