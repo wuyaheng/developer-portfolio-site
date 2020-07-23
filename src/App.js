c27d9d703b84f6878c12efa7bb7f278d5e4ca078
wuyaheng <wuyaheng2016@gmail.com>
2020-07-22 21:31:40 -0400

    create resume page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Portfolio from './pages/Portfolio/Portfolio';
-// import Resume from './pages/Resume/Resume';
+import Resume from './pages/Resume/Resume';
 import Blog from './pages/Blog/Blog';
 
 
@@ -15,7 +15,7 @@ function App() {
     <BrowserRouter>
       <Route exact path='/' component={Home}/>
       <Route path='/portfolio' component={Portfolio}/> 
-      {/* <Route path='/resume' component={Resume}/> */}
+      <Route path='/resume' component={Resume}/>
       <Route path='/blog' component={Blog}/> 
     </BrowserRouter> 
 
